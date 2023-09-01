9893e39531ffc94cee59a67dc404840bc56f3e78
KellyKiiru <infowithkiiru@gmail.com>
2023-09-01 22:53:57 +0300

    method to view images - has bugs

---
+++ src/app/view-image/view-image.component.ts
@@ -0,0 +1,54 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { ConfigService } from '../config.service';
+import { SettingsService } from '../settings.service';
+
+@Component({
+  selector: 'app-view-image',
+  templateUrl: './view-image.component.html',
+  styleUrls: ['./view-image.component.css']
+})
+export class ViewImageComponent implements OnInit{
+
+  // constructor(private httpClient:HttpClient) {};
+
+  name:string = 'Test display image';
+  thumbnail: any;
+  constructor(private config: ConfigService,public setting:SettingsService, private sanitizer: DomSanitizer, private httpClient:HttpClient) { }
+ 
+  ngOnInit() {
+     this.config.getData()
+      .subscribe((baseImage : any) => {
+        //alert(JSON.stringify(data.image));
+        let objectURL = 'data:image/jpeg;base64,' + baseImage.image;
+
+         this.thumbnail = this.sanitizer.bypassSecurityTrustUrl(objectURL);
+       
+      });
+    //console.log(this.setting.snippet)
+    
+  }
+
+  uploadedImage!: File;
+  dbImage:any;
+  postResponse:any;
+  successResponse!: string;
+  image!: string;
+
+  viewImage() {
+    return this.httpClient.get(`http://127.0.0.1:8000/get/image/info/${this.image}/`,{observe:'response'}).subscribe(res => {
+          console.log(res);
+          this.postResponse = res;
+          this.dbImage = 'data:image/jpeg;base64,'+ this.postResponse.image;
+          console.log("fdbniisanvjk");
+          console.log(this.dbImage);
+        }
+      );
+  };
+  
+  // ngOnInit(){
+  //   this.viewImage();
+  // }
+
+}
