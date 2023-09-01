b1743f5370a578a3ec2398ccbced5a86f83b58a4
KellyKiiru <infowithkiiru@gmail.com>
2023-09-01 22:46:21 +0300

    define url paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ViewImageComponent } from './view-image/view-image.component';
+import { UploadImageComponent } from './upload-image/upload-image.component';
 
 const routes: Routes = [
+  {path:'view-image', component: ViewImageComponent},
+  {path:'upload-image', component:UploadImageComponent}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    initialNavigation: 'enabledBlocking'
+})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
