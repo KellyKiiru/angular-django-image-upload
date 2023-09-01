4499f73741fd6d764a1bb5cee55585691946088a
KellyKiiru <infowithkiiru@gmail.com>
2023-09-01 03:45:45 +0300

    define functions to upload image and view image

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpEventType } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'angular-image-upload';
+
+  constructor(private httpClient:HttpClient) {}
+
+  uploadedImage!: File;
+  dbImage:any;
+  postResponse:any;
+  successResponse!: string;
+  image:any;
+
+  public onImageUpload(event:any){
+    this.uploadedImage = event.target.files[0]
+  }
+
+  imageUploadAction(){
+    const imageFormData = new FormData();
+    imageFormData.append('image',this.uploadedImage, this.uploadedImage.name)
+
+    this.httpClient.post('http://localhost:8000/upload/image/',imageFormData,{observe:'response'}).subscribe((response)=>{
+      if (response.status === 200){
+        this.postResponse = response;
+        this.successResponse = this.postResponse.body.message;
+      } else {
+        this.successResponse = 'Image not uploaded due to some error'
+      }
+    });
+  }
+
+  viewImage() {
+    this.httpClient.get('http://localhost:8080/get/image/info/' + this.image).subscribe(
+        res => {
+          this.postResponse = res;
+          this.dbImage = 'data:image/jpeg;base64,' + this.postResponse.image;
+        }
+      );
+  }
+
+  ngOnInit(): void {
+    this.imageUploadAction();
+    this.viewImage();
+  }
 }
