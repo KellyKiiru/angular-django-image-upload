12018ab0d59c38393f599a7d196c973079b77844
KellyKiiru <infowithkiiru@gmail.com>
2023-09-04 12:25:02 +0300

    define ngOnInit method

--- src/app/components/create-profile/create-profile.component.ts
+++ src/app/components/create-profile/create-profile.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+
+import { Profile } from 'src/app/models/profiles';
 
 @Component({
   selector: 'app-create-profile',
   templateUrl: './create-profile.component.html',
   styleUrls: ['./create-profile.component.css']
 })
-export class CreateProfileComponent {
+export class CreateProfileComponent implements OnInit {
 
+  form!: FormGroup;
+  profile!:Profile;
+  imageData!:string;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      name : new FormControl(null),
+      image : new FormControl(null),
+    })
+  }
 }
