cfc41b58e4b5b4e37e2440708bf19211ce90db4f
KellyKiiru <infowithkiiru@gmail.com>
2023-09-04 12:12:38 +0300

    define url paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ViewImageComponent } from './view-image/view-image.component';
 import { UploadImageComponent } from './upload-image/upload-image.component';
+import { CreateProfileComponent } from './components/create-profile/create-profile.component';
+import { AllProfilesComponent } from './components/all-profiles/all-profiles.component';
 
 const routes: Routes = [
   {path:'view-image', component: ViewImageComponent},
-  {path:'upload-image', component:UploadImageComponent}
+  {path:'upload-image', component:UploadImageComponent},
+  {path:'create-profile', component:CreateProfileComponent},
+  {path:'profiles', component:AllProfilesComponent}
 ];
 
 @NgModule({
