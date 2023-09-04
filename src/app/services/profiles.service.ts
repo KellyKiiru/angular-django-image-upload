f8190516c13ef791540ae1c4076c8d61e820513d
KellyKiiru <infowithkiiru@gmail.com>
2023-09-04 14:03:59 +0300

    define method for fetching profiles from the server in profile.service.ts

--- src/app/services/profiles.service.ts
+++ src/app/services/profiles.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { Profile } from '../models/profiles';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfilesService {
+  private profiles: Profile[] =[];
 
-  constructor() { }
+  private profiles$ = new Subject<Profile[]>()
+
+  readonly url = 'http://127.0.0.1:8000/api/profiles';
+
+  constructor(private http:HttpClient) { }
+
+  getProfiles(){
+    this.http.get<{ profiles : Profile[] }>(this.url).pipe(
+      map((profileData) => {
+        return profileData.profiles
+      })
+    ).subscribe((profiles)=>{
+        this.profiles = profiles;
+    })
+  }
+
+  
 }
