bdbcc2f63ba6ad582479621524c63a6917711701
KellyKiiru <infowithkiiru@gmail.com>
2023-09-04 22:50:53 +0300

    define methods in all-profiles.component.ts

--- src/app/components/all-profiles/all-profiles.component.ts
+++ src/app/components/all-profiles/all-profiles.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { Profile } from 'src/app/models/profiles';
+import { ProfilesService } from 'src/app/services/profiles.service';
 
 @Component({
   selector: 'app-all-profiles',
   templateUrl: './all-profiles.component.html',
   styleUrls: ['./all-profiles.component.css']
 })
-export class AllProfilesComponent {
+export class AllProfilesComponent implements OnInit, OnDestroy {
 
+  profiles: Profile[] =[]
+
+ 
+
+ constructor(private profileService : ProfilesService,  private profileSubscription:Subscription){}
+
+    ngOnInit(): void {
+      this.profileService.getProfiles();
+      this.profileSubscription = this.profileService.getProfilesStream().subscribe((profiles:Profile [])=>{
+        this.profiles = profiles
+      })
+    }
+
+    ngOnDestroy(): void {
+      this.profileSubscription.unsubscribe();
+    }
 }
